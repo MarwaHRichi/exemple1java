7ac241eea9733d2fc4c892c7b7a1b0291ca4d77c
MarwaHRichi <marwahrichi93@gmail.com>
2022-01-19 12:16:24 +0100

    scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,8 +9,21 @@ public class Main {
     User user2 = new User("Ahmed", "Hr", "ahmed@yahoo", "123", 5);
     System.out.println(user1);
     System.out.println(user2);
+        Scanner scan = new Scanner(System.in);
+        String firstname;
+        String lastname;
+        String email;
+        int age;
+        System.out.println("Entrer Firstname");
+        firstname = scan.nextLine();
+        System.out.println("Entrer Lastname");
+        lastname = scan.nextLine();
+        System.out.println("Entrer email");
+        email = scan.nextLine();
+        System.out.println("Entrer age");
+        age = Integer.parseInt(scan.nextLine());
 
-
+        System.out.println("L'utilisateur est :" +firstname+ " " +lastname+ " son email est :"+email+ " et il a " +age+ " ans");
     }
 
 }
