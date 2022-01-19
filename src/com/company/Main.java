ebbde37d919eae5ee96b362478e092870c0ac0bd
MarwaHRichi <marwahrichi93@gmail.com>
2022-01-19 11:33:35 +0100

    java

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	User user1 = new User("Marwa", "Hr", "marwa@gmail", "mmm", 28);
+    User user2 = new User("Ahmed", "Hr", "ahmed@yahoo", "123", 5);
+    System.out.println(user1);
+    System.out.println(user2);
+
+
+    }
+
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,71 @@
+package com.company;
+
+public class User {
+    String firstname;
+    String lastname;
+    String email;
+    String password;
+    int age;
+
+    public User() {
+    }
+
+    public User(String firstname, String lastname, String email, String password, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.age = age;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
