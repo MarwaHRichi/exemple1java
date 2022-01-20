6a3e3a78ae198c7a52541dc1500ece3d67c5c172
MarwaHRichi <marwahrichi93@gmail.com>
2022-01-20 10:43:07 +0100

    users

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
 	User user1 = new User("Marwa", "Hr", "marwa@gmail", "mmm", 28);
     User user2 = new User("Ahmed", "Hr", "ahmed@yahoo", "123", 5);
-    User user3 = new User("Meriem", "hr","meriemhr@gmail","1234",25);
+
     System.out.println(user1);
     System.out.println(user2);
-    System.out.println(user3);
+   // System.out.println(user3);
         Scanner scan = new Scanner(System.in);
         String firstname;
         String lastname;
@@ -24,8 +24,11 @@ public class Main {
         email = scan.nextLine();
         System.out.println("Entrer age");
         age = Integer.parseInt(scan.nextLine());
+        User user3 = new User("Meriem", "hr","meriemhr@gmail","1234",25);
+        //System.out.println("L'utilisateur est :" +firstname+ " " +lastname+ " son email est :"+email+ " et il a " +age+ " ans");
+        System.out.println("L'utilisateur est :" +user3.firstname+ " " +user3.lastname+ " son email est :"+user3.email+ " et il a " +user3.age+ " ans");
 
-        System.out.println("L'utilisateur est :" +firstname+ " " +lastname+ " son email est :"+email+ " et il a " +age+ " ans");
+        //System.out.println(user3);
 
 
     }
